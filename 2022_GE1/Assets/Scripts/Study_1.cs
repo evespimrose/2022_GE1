652ae29b343cf32f6866bc9999bb58294ac808df
evespimrose <bigjht34@gmail.com>
2022-03-11 12:21:53 +0900

    씬 생성, 스크립트 생성, 조명 설명, 기본 UI 설명 등

---
+++ 2022_GE1/Assets/Scripts/Study_1.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Study_1 : MonoBehaviour
+{
+    Rigidbody rigid;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
