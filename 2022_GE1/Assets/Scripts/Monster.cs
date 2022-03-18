7e8fa667ca5398b2de422daa9197eb7e21486060
evespimrose <bigjht34@gmail.com>
2022-03-18 12:23:49 +0900

    3주차_10. 임의 monobehavior 상속

---
+++ 2022_GE1/Assets/Scripts/Monster.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print(message:"'크아아' 투명몬스터가 울부짖었다");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
