25ae7d0cd7d33a4994bfeb804f5b2737bfa546ba
evespimrose <bigjht34@gmail.com>
2022-05-06 11:33:34 +0900

    10주차_3. FSM 선언 및 연결

--- 2022_GE1/Assets/3Scripts/0506/npc.cs
+++ 2022_GE1/Assets/3Scripts/0506/npc.cs
@@ -7,7 +7,7 @@ using UnityEngine.AI;
 public class npc : MonoBehaviour
 {
     private NavMeshAgent _agent;
-
+    private NpcState _npcState;
     public Transform target;
 
     private void Awake()
@@ -18,5 +18,49 @@ public class npc : MonoBehaviour
     private void Update()
     {
         _agent.destination = target.position;
+    }
+
+    private void OnEnable()
+    {
+        StartCoroutine(NpcRoutine());
+    }
+    private void OnDisable()
+    {
+        StopAllCoroutines();
+    }
+
+    IEnumerator NpcRoutine()
+    {
+        _npcState = NpcState.Finding;
+
+        while(true)
+        {
+            switch(_npcState)
+            {
+                case NpcState.Finding:
+                    yield return Find();
+                    break;
+                case NpcState.Chasing:
+                    yield return Chase();
+                    break;
+                case NpcState.Attacking:
+                    yield return Attack();
+                    break;
+            }
+            yield return null;
+        }
+    }
+
+    IEnumerator Find()
+    {
+        yield return null;
+    }
+    IEnumerator Chase()
+    {
+        yield return null;
+    }
+    IEnumerator Attack()
+    {
+        yield return null;
     }
 }
