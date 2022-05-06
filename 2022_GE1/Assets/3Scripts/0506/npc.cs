f47fae70b4c860197257a66803ede53c9e11a216
evespimrose <bigjht34@gmail.com>
2022-05-06 11:07:37 +0900

    10주차_2. NavMesh

---
+++ 2022_GE1/Assets/3Scripts/0506/npc.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class npc : MonoBehaviour
+{
+    private NavMeshAgent _agent;
+
+    public Transform target;
+
+    private void Awake()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        _agent.destination = target.position;
+    }
+}
